1c899835a1fe8a2329b636781ea06db98dc44466
Jason Preszler <jason@lightstrike.ai>
2025-12-30 10:53:26 -0800

    map states

--- stepscribe/states/map.py
+++ stepscribe/states/map.py
@@ -1,13 +1,34 @@
 from dataclasses import dataclass
 
 from .base_state import State
-
+from ..components import ItemProcessor, Retry, Catcher, ResultWriter, ItemReader
 
 @dataclass
 class DistributedMap(State):
-    pass
+    type_ = "Map"
+    item_processor: ItemProcessor
+    item_reader: ItemReader
+    items: list | None = None
+    item_batcher: list | None = None
+    max_concurrency: int | str | None = None
+    tolerated_failure_pct: int | str | None = None
+    tolerated_failure_count: int | str | None = None
+    label: str | None = None
+    result_writer: ResultWriter | None = None
+    result_selector: dict | None = None
+    retry: Retry | None = None
+    catch: list[Catcher] | None = None
+
+
 
 
 @dataclass
 class Map(State):
-    pass
+    type_ = "Map"
+    item_processor: ItemProcessor
+    items: list | str | None = None
+    item_selector: dict | None = None
+    max_concurrency: int | str | None = None
+    retry: Retry | None = None
+    catch: list[Catcher] | None = None
+
